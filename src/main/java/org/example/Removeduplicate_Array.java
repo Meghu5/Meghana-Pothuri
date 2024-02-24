dc92b47dfbfa822139726e660de9e5b22d20b4d8
Meghu5 <Kamakshi@1991>
2024-02-24 21:38:35 +0530

    code commit

---
+++ src/main/java/org/example/Removeduplicate_Array.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.*;
+public class Removeduplicate_Array {
+
+    public static void main(String args[]){
+
+
+
+    }
+
+    int arr[] ={1,2,3,4,5,6,3,2,2,2,2};
+
+
+}
